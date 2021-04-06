416f1272f07d9efaa2d26344acc55702005e14a1
Arto <aruthart@gmail.com>
2021-04-06 20:47:12 +0300

    VAT callback draft

--- vat/_nbdev.py
+++ vat/_nbdev.py
@@ -2,7 +2,8 @@
 
 __all__ = ["index", "modules", "custom_doc_links", "git_url"]
 
-index = {"ALUMCallback": "00_core.ipynb"}
+index = {"ALUMCallback": "00_core.ipynb",
+         "VATCallback": "00_core.ipynb"}
 
 modules = ["core.py"]
 
